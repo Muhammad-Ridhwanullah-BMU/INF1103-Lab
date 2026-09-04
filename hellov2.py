5f00313df3b592fc4020df8393591e7bbc02e395
Ridhwanullah <ridhwanullah2004@gmail.com>
2026-09-04 17:09:39 +0800

    Added Changes:\nconditional line if age > 40 will display statement regarding  OLD

--- hellov2.py
+++ hellov2.py
@@ -1,9 +1,12 @@
-username = str(input("Enter Username: "))
+username = input("Enter Username: ")
 age = int(input("Enter Age: "))
-category = str(input("Enter Content Category"))
+category = str(input("Enter Content Category: "))
 
 print("\nInstagram Profile")
 print("========================================")
 print(f"Username: {username}")
 print(f"Age: {age}")
 print(f"Category: {category}")
+
+if age > 40 and category.lower() == "fun":
+    print("You are old, what is fun for you??")
