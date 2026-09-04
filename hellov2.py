430cf780fc0df4676fca34cb4d4c1967e0ae8b09
Ridhwanullah <ridhwanullah2004@gmail.com>
2026-09-04 17:01:16 +0800

    hellov2.py is the sequel to heelo.py\nCurrently it has an interactive Instagram Profile displayer where user can enter their username, age, and content category then the program will display their information.

---
+++ hellov2.py
@@ -0,0 +1,9 @@
+username = str(input("Enter Username: "))
+age = int(input("Enter Age: "))
+category = str(input("Enter Content Category"))
+
+print("\nInstagram Profile")
+print("========================================")
+print(f"Username: {username}")
+print(f"Age: {age}")
+print(f"Category: {category}")
